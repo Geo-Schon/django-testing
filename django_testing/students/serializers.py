248b44ad08afe77ab4a7dce6d2b1904c37609adc
Geo <dr-gudkov@mail.ru>
2023-07-14 17:13:33 +0300

    Выполнено задание v1.0

---
+++ django_testing/students/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers, exceptions
+
+from django_testing import settings
+from students.models import Course
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = ("id", "name", "students")
+
+
+    def validate_students(self, value):
+        if len(value) > settings.MAX_STUDENTS_PER_COURSE:
+            raise exceptions.ValidationError(
+                f'Слишком много студентов на курсе: {settings.MAX_STUDENTS_PER_COURSE}'
+            )
+        return value
